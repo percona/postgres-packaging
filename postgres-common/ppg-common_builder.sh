23b61fa78fcc688a544118a6a4be379b52e761de
Evgeniy Patlan <evgeniy.patlan@percona.com>
2020-05-28 16:13:38 +0300

    [DISTPG-74] postgresql-common build fix

--- postgres-common/ppg-common_builder.sh
+++ postgres-common/ppg-common_builder.sh
@@ -118,11 +118,11 @@ get_sources(){
         for file in $(ls | grep ^postgresql); do 
             mv $file "percona-$file"
         done
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.2/postgres-common/control_common.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.2/postgres-common/maintscripts-functions.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.2/postgres-common/percona-postgresql-common.templates.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.2/postgres-common/rules_common.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.2/postgres-common/supported_versions.patch
+        wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.3/postgres-common/control_common.patch
+        wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.3/postgres-common/maintscripts-functions.patch
+        wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.3/postgres-common/percona-postgresql-common.templates.patch
+        wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.3/postgres-common/rules_common.patch
+        wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.3/postgres-common/supported_versions.patch
         patch -p0 < control_common.patch
         patch -p0 < maintscripts-functions.patch
         patch -p0 < percona-postgresql-common.templates.patch
@@ -145,10 +145,9 @@ get_sources(){
         for file in $(ls | grep postgresql); do
             mv $file "percona-$file"
         done
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.2/postgres-common/percona-postgresql-common.spec.patch
+        wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.3/postgres-common/percona-postgresql-common.spec.patch
         patch -p0 < percona-postgresql-common.spec.patch
         rm -rf percona-postgresql-common.spec.patch
-	sed -i 's:1%:2%:' percona-postgresql-common.spec
     cd ../
     cd ${WORKDIR}
     #
@@ -436,12 +435,12 @@ INSTALL=0
 RPM_RELEASE=1
 DEB_RELEASE=1
 REVISION=0
-BRANCH="214"
+BRANCH="215"
 REPO="https://salsa.debian.org/postgresql/postgresql-common.git"
 PRODUCT=percona-postgresql
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='214'
+VERSION='215'
 RELEASE='1'
 PRODUCT_FULL=${PRODUCT}-${VERSION}
 

a018748e55398a851ba66227de57e3ca8535d314
Evgeniy Patlan <evgeniy.patlan@percona.com>
2020-10-05 21:53:09 +0300

    DISTPG-133 postgresql-common-218 and pgbackrest

--- postgres-common/ppg-common_builder.sh
+++ postgres-common/ppg-common_builder.sh
@@ -118,17 +118,16 @@ get_sources(){
         for file in $(ls | grep ^postgresql); do 
             mv $file "percona-$file"
         done
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.4/postgres-common/control_common.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.4/postgres-common/maintscripts-functions.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.4/postgres-common/percona-postgresql-common.templates.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.4/postgres-common/rules_common.patch
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.4/postgres-common/supported_versions.patch
-        patch -p0 < control_common.patch
+	rm -rf rules control
+        wget https://raw.githubusercontent.com/percona/postgres-packaging/13.0/postgres-common/control
+        wget https://raw.githubusercontent.com/percona/postgres-packaging/13.0/postgres-common/maintscripts-functions.patch
+        wget https://raw.githubusercontent.com/percona/postgres-packaging/13.0/postgres-common/percona-postgresql-common.templates.patch
+        wget https://raw.githubusercontent.com/percona/postgres-packaging/13.0/postgres-common/rules
+	wget https://raw.githubusercontent.com/percona/postgres-packaging/13.0/postgres-common/supported_versions.patch
         patch -p0 < maintscripts-functions.patch
         patch -p0 < percona-postgresql-common.templates.patch
-        patch -p0 < rules_common.patch
         patch -p0 < supported_versions.patch
-        rm -rf control_common.patch maintscripts-functions.patch percona-postgresql-common.templates.patch rules_common.patch supported_versions.patch
+        rm -rf maintscripts-functions.patch percona-postgresql-common.templates.patch supported_versions.patch
         sed -i 's:postgresql-common:percona-postgresql-common:' percona-postgresql-common.preinst
         sed -i 's:postgresql-common:percona-postgresql-common:' percona-postgresql-common.postrm
 	    sed -i 's:db_get postgresql-common:db_get percona-postgresql-common:' percona-postgresql-common.postinst
@@ -138,17 +137,17 @@ get_sources(){
         echo "  * Initial Release." >> changelog
         echo " -- EvgeniyPatlan <evgeniy.patlan@percona.com> $(date -R)" >> changelog
         sed -i 's:percona-postgresql-plpython-$v,::' rules
-        sed -i 's:"10":"12":' supported-versions
-        sed -i 's:"11":"12":' supported-versions
+        sed -i 's:"9.6":"13":' supported-versions
+        sed -i 's:"10":"13":' supported-versions
+        sed -i 's:"11":"13":' supported-versions
+        sed -i 's:"12":"13":' supported-versions
     cd ../
     cd rpm
         for file in $(ls | grep postgresql); do
             mv $file "percona-$file"
         done
-        wget https://raw.githubusercontent.com/percona/postgres-packaging/12.4/postgres-common/percona-postgresql-common.spec.patch
-        patch -p0 < percona-postgresql-common.spec.patch
-        rm -rf percona-postgresql-common.spec.patch
-	sed -i 's:1%:3%:' percona-postgresql-common.spec
+	rm -rf percona-postgresql-common.spec
+        wget https://raw.githubusercontent.com/percona/postgres-packaging/13.0/postgres-common/percona-postgresql-common.spec
     cd ../
     cd ${WORKDIR}
     #
@@ -156,7 +155,7 @@ get_sources(){
     #
 
     tar --owner=0 --group=0 --exclude=.* -czf ${PRODUCT_FULL}.tar.gz ${PRODUCT_FULL}
-    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}-12/${PRODUCT_FULL}/${BRANCH}/${REVISION}/${BUILD_ID}" >> percona-postgresql.properties
+    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}-13/${PRODUCT_FULL}/${BRANCH}/${REVISION}/${BUILD_ID}" >> percona-postgresql.properties
     mkdir $WORKDIR/source_tarball
     mkdir $CURDIR/source_tarball
     cp ${PRODUCT_FULL}.tar.gz $WORKDIR/source_tarball
@@ -436,13 +435,13 @@ INSTALL=0
 RPM_RELEASE=1
 DEB_RELEASE=1
 REVISION=0
-BRANCH="216"
+BRANCH="219"
 REPO="https://salsa.debian.org/postgresql/postgresql-common.git"
 PRODUCT=percona-postgresql
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='216'
-RELEASE='3'
+VERSION='219'
+RELEASE='1'
 PRODUCT_FULL=${PRODUCT}-${VERSION}
 
 check_workdir

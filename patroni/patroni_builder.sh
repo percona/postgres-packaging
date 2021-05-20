b0c300127df868f58370cc556e059df1a0727cca
Evgeniy Patlan <evgeniy.patlan@percona.com>
2021-05-20 08:54:05 +0300

    Update patroni

--- patroni/patroni_builder.sh
+++ patroni/patroni_builder.sh
@@ -81,7 +81,7 @@ add_percona_yum_repo(){
     fi
     yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
     percona-release disable all
-    percona-release enable ppg-12.6 testing
+    percona-release enable ppg-12.7 testing
     return
 }
 
@@ -90,7 +90,7 @@ add_percona_apt_repo(){
     dpkg -i percona-release_latest.generic_all.deb
     rm -f percona-release_latest.generic_all.deb
     percona-release disable all
-    percona-release enable ppg-12.6 testing
+    percona-release enable ppg-12.7 testing
     return
 }
 
@@ -135,10 +135,10 @@ get_sources(){
     mv all_packaging/DEB/debian ./
     cd debian
     rm -f rules
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/12/patroni/rules
+    wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.7/patroni/rules
     rm -f control
     rm -f postinst
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/12/patroni/control
+    wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.7/patroni/control
     sed -i 's:service-info-only-in-pretty-format.patch::' patches/series
     sed -i 's:patronictl-reinit-wait-rebased-1.6.0.patch::' patches/series
     mv install percona-patroni.install
@@ -150,7 +150,7 @@ get_sources(){
     mv all_packaging/RPM/* rpm/
     cd rpm
     rm -f patroni.spec
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/12/patroni/patroni.spec
+    wget https://raw.githubusercontent.com/EvgeniyPatlan/postgres-packaging/12.7/patroni/patroni.spec
     sed -i 's:/opt/app:/opt:g' patroni.2.service
     mv patroni.2.service patroni.service
     tar -czf patroni-customizations.tar.gz patroni.service patroni-watchdog.service postgres-telia.yml
@@ -484,8 +484,8 @@ OS_NAME=
 ARCH=
 OS=
 INSTALL=0
-RPM_RELEASE=2
-DEB_RELEASE=2
+RPM_RELEASE=3
+DEB_RELEASE=3
 REVISION=0
 BRANCH="v2.0.2"
 REPO="https://github.com/zalando/patroni.git"
@@ -493,7 +493,7 @@ PRODUCT=percona-patroni
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
 VERSION='2.0.2'
-RELEASE='2'
+RELEASE='3'
 PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
 
 check_workdir

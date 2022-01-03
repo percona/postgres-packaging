572a22bf12fad9a2b23a9a2d1112f9bdc93a4075
Vadim Yalovets <vadim.yalovets@percona.com>
2022-01-03 17:24:57 +0200

    DISTPG-345 Re-installation of percona-pgbouncer issues

--- pgbouncer/pgbouncer_builder.sh
+++ pgbouncer/pgbouncer_builder.sh
@@ -137,6 +137,7 @@ get_sources(){
     rm -f control rules
     wget https://raw.githubusercontent.com/percona/postgres-packaging/12.9/pgbouncer/control
     wget https://raw.githubusercontent.com/percona/postgres-packaging/12.9/pgbouncer/rules
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/12.9/pgbouncer/preinst
     echo 9 > compat
     cd ../
     rm -rf deb_packaging

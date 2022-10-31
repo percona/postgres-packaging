96e1fa36cc2be5597a462bb4ac63e62dcaa2f8b2
Surabhi Bhat <surabhi.bhat@percona.com>
2022-10-31 17:22:30 +0530

    DISTPG-465 Add ppg-server-ha_builder.sh, ppg-server_builder.sh and rules file for debian builds and update spec file for ppg-server-ha and ppg-server meta-package builds

--- ppg-server-ha/ppg-server-ha.spec
+++ ppg-server-ha/ppg-server-ha.spec
@@ -1,10 +1,10 @@
 %global sname   percona-ppg-server-ha
 %global pgmajorversion 13
-%global version 7
+%global version 8
 
 Summary:        Percona selection of PostgreSQL%{pgmajorversion} HA components
 Name:           %{sname}%{pgmajorversion}
-Version:        %{version}
+Version:        %{pgmajorversion}.%{version}
 Release:        1%{?dist}
 License:        PostgreSQL
 Group:          Applications/Databases
@@ -25,5 +25,7 @@ from the community, tested to work together in demanding enterprise environments
 %files
 
 %changelog
+* Mon Oct 31 2022 Surabhi Bhat <surabhi.bhat> 13.8-1
+- Update version for ppg-server-ha meta-package
 * Mon Aug 08 2022 Kai Wagner <kai.wagner@percona.com> 13.7-1
 - Initial build
---
+++ ppg-server-ha/ppg-server-ha_builder.sh
@@ -0,0 +1,458 @@
+#!/usr/bin/env bash
+
+shell_quote_string() {
+  echo "$1" | sed -e 's,\([^a-zA-Z0-9/_.=-]\),\\\1,g'
+}
+
+usage () {
+    cat <<EOF
+Usage: $0 [OPTIONS]
+    The following options may be given :
+        --builddir=DIR      Absolute path to the dir where all actions will be performed
+        --get_sources       Source will be downloaded from github
+        --build_src_rpm     If it is set - src rpm will be built
+        --build_src_deb  If it is set - source deb package will be built
+        --build_rpm         If it is set - rpm will be built
+        --build_deb         If it is set - deb will be built
+        --install_deps      Install build dependencies(root privilages are required)
+        --branch            Branch for build
+        --repo              Repo for build
+        --version           Packaging repo
+        --help) usage ;;
+Example $0 --builddir=/tmp/BUILD --get_sources=1 --build_src_rpm=1 --build_rpm=1
+EOF
+        exit 1
+}
+
+append_arg_to_args () {
+  args="$args "$(shell_quote_string "$1")
+}
+
+parse_arguments() {
+    pick_args=
+    if test "$1" = PICK-ARGS-FROM-ARGV
+    then
+        pick_args=1
+        shift
+    fi
+
+    for arg do
+        val=$(echo "$arg" | sed -e 's;^--[^=]*=;;')
+        case "$arg" in
+            --builddir=*) WORKDIR="$val" ;;
+            --build_src_rpm=*) SRPM="$val" ;;
+            --build_src_deb=*) SDEB="$val" ;;
+            --build_rpm=*) RPM="$val" ;;
+            --build_deb=*) DEB="$val" ;;
+            --get_sources=*) SOURCE="$val" ;;
+            --branch=*) BRANCH="$val" ;;
+            --repo=*) REPO="$val" ;;
+            --version=*) VERSION=$(echo $val|awk -F'-' '{print $2}') ;;
+            --install_deps=*) INSTALL="$val" ;;
+            --help) usage ;;
+            *)
+              if test -n "$pick_args"
+              then
+                  append_arg_to_args "$arg"
+              fi
+              ;;
+        esac
+    done
+}
+
+check_workdir(){
+    if [ "x$WORKDIR" = "x$CURDIR" ]
+    then
+        echo >&2 "Current directory cannot be used for building!"
+        exit 1
+    else
+        if ! test -d "$WORKDIR"
+        then
+            echo >&2 "$WORKDIR is not a directory."
+            exit 1
+        fi
+    fi
+    return
+}
+
+add_percona_yum_repo(){
+    yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
+    percona-release disable all
+    percona-release enable ppg-13.8 testing
+    return
+}
+
+add_percona_apt_repo(){
+    wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
+    dpkg -i percona-release_latest.generic_all.deb
+    rm -f percona-release_latest.generic_all.deb
+    percona-release disable all
+    percona-release enable ppg-13.8 testing
+    return
+}
+
+get_sources(){
+    cd "${WORKDIR}"
+    if [ "${SOURCE}" = 0 ]
+    then
+        echo "Sources will not be downloaded"
+        return 0
+    fi
+    PRODUCT=percona-ppg-server-ha-13
+    echo "PRODUCT=${PRODUCT}" > ppg-server-ha.properties
+
+    PRODUCT_FULL=${PRODUCT}-${VERSION}
+    echo "PRODUCT_FULL=${PRODUCT_FULL}" >> ppg-server-ha.properties
+    echo "VERSION=${PSM_VER}" >> ppg-server-ha.properties
+    echo "BUILD_NUMBER=${BUILD_NUMBER}" >> ppg-server-ha.properties
+    echo "BUILD_ID=${BUILD_ID}" >> ppg-server-ha.properties
+    git clone "$REPO" ${PRODUCT_FULL}
+    retval=$?
+    if [ $retval != 0 ]
+    then
+        echo "There were some issues during repo cloning from github. Please retry one more time"
+        exit 1
+    fi
+    cd ${PRODUCT_FULL}
+    if [ ! -z "$BRANCH" ]
+    then
+        git reset --hard
+        git clean -xdf
+        git checkout "$BRANCH"
+        git submodule update --init
+    fi
+    REVISION=$(git rev-parse --short HEAD)
+    echo "REVISION=${REVISION}" >> ${WORKDIR}/ppg-server-ha.properties
+
+    mkdir debian
+    cd debian/
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.8/ppg-server-ha/control
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.8/ppg-server-ha/rules
+    echo 9 > compat
+    echo "percona-ppg-server-ha-13 (${VERSION}-${RELEASE}) unstable; urgency=low" >> changelog
+    echo "  * Initial Release." >> changelog
+    echo " -- SurabhiBhat <surabhi.bhat@percona.com> $(date -R)" >> changelog
+
+    cd ../
+    mkdir rpm
+    cd rpm
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.8/ppg-server-ha/ppg-server-ha.spec
+    cd ${WORKDIR}
+    #
+    source ppg-server-ha.properties
+    #
+
+    tar --owner=0 --group=0 --exclude=.* -czf ${PRODUCT_FULL}.tar.gz ${PRODUCT_FULL}
+    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}/${PRODUCT_FULL}/${PSM_BRANCH}/${REVISION}/${BUILD_ID}" >> ppg-server-ha.properties
+    mkdir $WORKDIR/source_tarball
+    mkdir $CURDIR/source_tarball
+    cp ${PRODUCT_FULL}.tar.gz $WORKDIR/source_tarball
+    cp ${PRODUCT_FULL}.tar.gz $CURDIR/source_tarball
+    cd $CURDIR
+    rm -rf percona-ppg-server-ha*
+    return
+}
+
+get_system(){
+    if [ -f /etc/redhat-release ]; then
+        RHEL=$(rpm --eval %rhel)
+        ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+        OS_NAME="el$RHEL"
+        OS="rpm"
+    else
+        ARCH=$(uname -m)
+        apt-get -y update
+        apt-get -y install lsb-release
+        OS_NAME="$(lsb_release -sc)"
+        OS="deb"
+    fi
+    return
+}
+
+switch_to_vault_repo() {
+     sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+     sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+}
+
+install_deps() {
+    if [ $INSTALL = 0 ]; then
+        echo "Dependencies will not be installed"
+        return;
+    fi
+    if [ $( id -u ) -ne 0 ]; then
+        echo "It is not possible to instal dependencies. Please run as root"
+        exit 1
+    fi
+    CURPLACE=$(pwd)
+
+    if [ "x$OS" = "xrpm" ]; then
+      RHEL=$(rpm --eval %rhel)
+      yum -y install wget
+      add_percona_yum_repo
+      yum clean all
+
+      if [ ${RHEL} = 8 ]; then
+          dnf -y module disable postgresql
+          dnf config-manager --set-enabled codeready-builder-for-rhel-8-x86_64-rpms
+          dnf clean all
+          rm -r /var/cache/dnf
+          dnf -y upgrade
+          yum -y install perl lz4-libs c-ares-devel
+      fi
+      INSTALL_LIST="git rpm-build rpmdevtools wget rpmlint"
+      yum -y install ${INSTALL_LIST}
+      yum -y install lz4 || true
+
+    else
+      export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+      apt-get -y update
+      apt-get -y install wget curl lsb-release gnupg2
+      export DEBIAN=$(lsb_release -sc)
+      add_percona_apt_repo
+      apt-get update || true
+      INSTALL_LIST="debconf debhelper devscripts dh-exec git"
+      until DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated install ${INSTALL_LIST}; do
+        sleep 1
+        echo "waiting"
+      done
+    fi
+    return;
+}
+
+get_tar(){
+    TARBALL=$1
+    TARFILE=$(basename $(find $WORKDIR/$TARBALL -name 'percona-ppg-server-ha*.tar.gz' | sort | tail -n1))
+    if [ -z $TARFILE ]
+    then
+        TARFILE=$(basename $(find $CURDIR/$TARBALL -name 'percona-ppg-server-ha*.tar.gz' | sort | tail -n1))
+        if [ -z $TARFILE ]
+        then
+            echo "There is no $TARBALL for build"
+            exit 1
+        else
+            cp $CURDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
+        fi
+    else
+        cp $WORKDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
+    fi
+    return
+}
+
+get_deb_sources(){
+    param=$1
+    echo $param
+    FILE=$(basename $(find $WORKDIR/source_deb -name "percona-ppg-server-ha*.$param" | sort | tail -n1))
+    if [ -z $FILE ]
+    then
+        FILE=$(basename $(find $CURDIR/source_deb -name "percona-ppg-server-ha*.$param" | sort | tail -n1))
+        if [ -z $FILE ]
+        then
+            echo "There is no sources for build"
+            exit 1
+        else
+            cp $CURDIR/source_deb/$FILE $WORKDIR/
+        fi
+    else
+        cp $WORKDIR/source_deb/$FILE $WORKDIR/
+    fi
+    return
+}
+
+build_srpm(){
+    if [ $SRPM = 0 ]
+    then
+        echo "SRC RPM will not be created"
+        return;
+    fi
+    if [ "x$OS" = "xdeb" ]
+    then
+        echo "It is not possible to build src rpm here"
+        exit 1
+    fi
+    cd $WORKDIR
+    get_tar "source_tarball"
+    rm -fr rpmbuild
+    ls | grep -v tar.gz | xargs rm -rf
+    TARFILE=$(find . -name 'percona-ppg-server-ha*.tar.gz' | sort | tail -n1)
+    SRC_DIR=${TARFILE%.tar.gz}
+    #
+    mkdir -vp rpmbuild/{SOURCES,SPECS,BUILD,SRPMS,RPMS}
+    tar vxzf ${WORKDIR}/${TARFILE} --wildcards '*/rpm' --strip=1
+    #
+    cp -av rpm/* rpmbuild/SOURCES
+    cp -av rpm/ppg-server-ha.spec rpmbuild/SPECS
+    #
+    mv -fv ${TARFILE} ${WORKDIR}/rpmbuild/SOURCES
+    rpmbuild -bs --define "_topdir ${WORKDIR}/rpmbuild" --define "dist .generic" \
+        --define "version ${VERSION}" rpmbuild/SPECS/ppg-server-ha.spec
+    mkdir -p ${WORKDIR}/srpm
+    mkdir -p ${CURDIR}/srpm
+    cp rpmbuild/SRPMS/*.src.rpm ${CURDIR}/srpm
+    cp rpmbuild/SRPMS/*.src.rpm ${WORKDIR}/srpm
+    return
+}
+
+build_rpm(){
+    if [ $RPM = 0 ]
+    then
+        echo "RPM will not be created"
+        return;
+    fi
+    if [ "x$OS" = "xdeb" ]
+    then
+        echo "It is not possible to build rpm here"
+        exit 1
+    fi
+    SRC_RPM=$(basename $(find $WORKDIR/srpm -name 'percona-ppg-server-ha*.src.rpm' | sort | tail -n1))
+    if [ -z $SRC_RPM ]
+    then
+        SRC_RPM=$(basename $(find $CURDIR/srpm -name 'percona-ppg-server-ha*.src.rpm' | sort | tail -n1))
+        if [ -z $SRC_RPM ]
+        then
+            echo "There is no src rpm for build"
+            echo "You can create it using key --build_src_rpm=1"
+            exit 1
+        else
+            cp $CURDIR/srpm/$SRC_RPM $WORKDIR
+        fi
+    else
+        cp $WORKDIR/srpm/$SRC_RPM $WORKDIR
+    fi
+    cd $WORKDIR
+    rm -fr rpmbuild
+    mkdir -vp rpmbuild/{SOURCES,SPECS,BUILD,SRPMS,RPMS}
+    cp $SRC_RPM rpmbuild/SRPMS/
+
+    cd rpmbuild/SRPMS/
+    #
+    cd $WORKDIR
+    RHEL=$(rpm --eval %rhel)
+    ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+    export LIBPQ_DIR=/usr/pgsql-13/
+    export LIBRARY_PATH=/usr/pgsql-13/lib/:/usr/pgsql-13/include/
+    rpmbuild --define "_topdir ${WORKDIR}/rpmbuild" --define "dist .$OS_NAME" --define "version ${VERSION}" --rebuild rpmbuild/SRPMS/$SRC_RPM
+
+    return_code=$?
+    if [ $return_code != 0 ]; then
+        exit $return_code
+    fi
+    mkdir -p ${WORKDIR}/rpm
+    mkdir -p ${CURDIR}/rpm
+    cp rpmbuild/RPMS/*/*.rpm ${WORKDIR}/rpm
+    cp rpmbuild/RPMS/*/*.rpm ${CURDIR}/rpm
+}
+
+build_source_deb(){
+    if [ $SDEB = 0 ]
+    then
+        echo "source deb package will not be created"
+        return;
+    fi
+    if [ "x$OS" = "xrpm" ]
+    then
+        echo "It is not possible to build source deb here"
+        exit 1
+    fi
+    #rm -rf percona-ppg-server-ha*
+    get_tar "source_tarball"
+    rm -f *.dsc *.orig.tar.gz *.debian.tar.gz *.changes
+    #
+    TARFILE=$(basename $(find . -name 'percona-ppg-server-ha*.tar.gz' | sort | tail -n1))
+    DEBIAN=$(lsb_release -sc)
+    ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+    tar zxf ${TARFILE}
+    BUILDDIR=${TARFILE%.tar.gz}
+    #
+
+    
+    mv ${TARFILE} ${PRODUCT}_${VERSION}.orig.tar.gz
+    cd ${BUILDDIR}    
+    dch -D unstable --force-distribution -v "${VERSION}-${RELEASE}" "Update to new ppg-server-ha version ${VERSION}"
+    dpkg-buildpackage -S
+    cd ../
+    mkdir -p $WORKDIR/source_deb
+    mkdir -p $CURDIR/source_deb
+    cp *_source.changes $WORKDIR/source_deb
+    cp *.dsc $WORKDIR/source_deb
+    cp *.orig.tar.gz $WORKDIR/source_deb
+    cp *_source.changes $CURDIR/source_deb
+    cp *.dsc $CURDIR/source_deb
+    cp *.orig.tar.gz $CURDIR/source_deb
+}
+
+build_deb(){
+    if [ $DEB = 0 ]
+    then
+        echo "source deb package will not be created"
+        return;
+    fi
+    if [ "x$OS" = "xrmp" ]
+    then
+        echo "It is not possible to build source deb here"
+        exit 1
+    fi
+    #for file in 'dsc' 'orig.tar.gz' 'changes' 'debian.tar*'
+    for file in 'dsc' 'orig.tar.gz' 'changes'
+    do
+        get_deb_sources $file
+    done
+    cd $WORKDIR
+    TARFILE=$(basename $(find . -name 'percona-ppg-server-ha*.tar.gz' | sort | tail -n1))
+    tar zxf ${TARFILE}
+    rm -fv *.deb
+    #
+    export DEBIAN=$(lsb_release -sc)
+    export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+    #
+    echo "DEBIAN=${DEBIAN}" >> ppg-server-ha.properties
+    echo "ARCH=${ARCH}" >> ppg-server-ha.properties
+
+    #
+    DSC=$(basename $(find . -name '*.dsc' | sort | tail -n1))
+    #
+    dpkg-source -x ${DSC}
+    #
+    cd ${PRODUCT}-${VERSION}
+    dch -m -D "${DEBIAN}" --force-distribution -v "1:${VERSION}-${RELEASE}.${DEBIAN}" 'Update distribution'
+    unset $(locale|cut -d= -f1)
+    dpkg-buildpackage -rfakeroot -us -uc -b
+    mkdir -p $CURDIR/deb
+    mkdir -p $WORKDIR/deb
+    cp $WORKDIR/*.*deb $WORKDIR/deb
+    cp $WORKDIR/*.*deb $CURDIR/deb
+}
+#main
+
+CURDIR=$(pwd)
+VERSION_FILE=$CURDIR/ppg-server-ha.properties
+args=
+WORKDIR=
+SRPM=0
+SDEB=0
+RPM=0
+DEB=0
+SOURCE=0
+OS_NAME=
+ARCH=
+OS=
+INSTALL=0
+RPM_RELEASE=1
+DEB_RELEASE=1
+REVISION=0
+BRANCH="v13.8"
+REPO="https://github.com/percona/postgres-packaging.git"
+PRODUCT=percona-ppg-server-ha-13
+DEBUG=0
+VERSION='ppg-13.8'
+parse_arguments PICK-ARGS-FROM-ARGV "$@"
+RELEASE='1'
+PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
+
+check_workdir
+get_system
+install_deps
+get_sources
+build_srpm
+build_source_deb
+build_rpm
+build_deb

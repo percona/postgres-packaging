5f18e161f104ad7bc49de1a01e6459d19da2782e
Evgeniy Patlan <evgeniy.patlan@percona.com>
2022-11-21 15:46:31 +0200

    DISTPG-513 13.9

--- pg_repack/pgrepack_builder.sh
+++ pg_repack/pgrepack_builder.sh
@@ -77,11 +77,11 @@ add_percona_yum_repo(){
     if [ ! -f /etc/yum.repos.d/percona-dev.repo ]
     then
       wget http://jenkins.percona.com/yum-repo/percona-dev.repo
-      mv -f percona-dev.repo /etc/yum.repos.d/
+      #mv -f percona-dev.repo /etc/yum.repos.d/
     fi
     yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
     percona-release disable all
-    percona-release enable ppg-13.7 testing
+    percona-release enable ppg-13.9 testing
     return
 }
 
@@ -89,7 +89,7 @@ add_percona_apt_repo(){
     wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
     dpkg -i percona-release_latest.generic_all.deb
     percona-release disable all
-    percona-release enable ppg-13.7 testing
+    percona-release enable ppg-13.9 testing
     return
 }
 
@@ -130,10 +130,10 @@ get_sources(){
       git checkout -b percona-pg_repack debian/${VERSION}-${RELEASE}
     cd ../
     mv deb_packaging/debian ./
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/Makefile.patch
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/rules
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/control
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/control.in
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/Makefile.patch
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/rules
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/control
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/control.in
     patch -p0 < Makefile.patch
     rm -rf Makefile.patch
     cd debian
@@ -146,10 +146,10 @@ get_sources(){
     rm -rf deb_packaging
     mkdir rpm
     cd rpm
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/pg_repack.spec
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/pg_repack-pg13-makefile-pgxs.patch
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/pg_repack.spec
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/pg_repack-pg13-makefile-pgxs.patch
     cd ../
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.7/pg_repack/make.patch
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/13.9/pg_repack/make.patch
     patch -p0 < make.patch
     rm -f make.patch
     cd ${WORKDIR}
@@ -199,7 +199,7 @@ install_deps() {
       yum -y install wget
       add_percona_yum_repo
       wget http://jenkins.percona.com/yum-repo/percona-dev.repo
-      mv -f percona-dev.repo /etc/yum.repos.d/
+      #mv -f percona-dev.repo /etc/yum.repos.d/
       yum clean all
       RHEL=$(rpm --eval %rhel)
       if [ x"$RHEL" = x6 -o x"$RHEL" = x7 ]; then
@@ -456,16 +456,16 @@ OS_NAME=
 ARCH=
 OS=
 INSTALL=0
-RPM_RELEASE=3
-DEB_RELEASE=3
+RPM_RELEASE=1
+DEB_RELEASE=1
 REVISION=0
-BRANCH="ver_1.4.7"
+BRANCH="ver_1.4.8"
 REPO="https://github.com/reorg/pg_repack.git"
 PRODUCT=percona-pg_repack
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='1.4.7'
-RELEASE='3'
+VERSION='1.4.8'
+RELEASE='1'
 PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
 
 check_workdir
--- postgres/percona-postgresql-13.spec
+++ postgres/percona-postgresql-13.spec
@@ -77,14 +77,14 @@
 
 Summary:        PostgreSQL client programs and libraries
 Name:           percona-postgresql%{pgmajorversion}
-Version:        13.7
-Release:        3%{?dist}
+Version:        13.9
+Release:        1%{?dist}
 License:        PostgreSQL
 Url:            https://www.postgresql.org/
 Packager:       Percona Development Team <https://jira.percona.com>
 Vendor:         Percona, LLC
 
-Source0:        percona-postgresql-13.7.tar.gz
+Source0:        percona-postgresql-13.9.tar.gz
 Source4:        %{sname}-%{pgmajorversion}-Makefile.regress
 Source5:        %{sname}-%{pgmajorversion}-pg_config.h
 Source6:        %{sname}-%{pgmajorversion}-README-systemd.rpm-dist

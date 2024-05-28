96e16cafc61992c84fbb6ff764e49f856fb475b2
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-05-28 12:33:37 +0500

    Temporary commit to install packages from laboratory repo.

--- pgaudit/pgaudit_builder.sh
+++ pgaudit/pgaudit_builder.sh
@@ -79,7 +79,7 @@ add_percona_yum_repo(){
     mv percona-release.sh /usr/bin/percona-release
     chmod 777 /usr/bin/percona-release
     percona-release disable all
-    percona-release enable ppg-${PG_VERSION} testing
+    percona-release enable ppg-${PG_VERSION} laboratory
     return
 }
 
@@ -88,7 +88,7 @@ add_percona_apt_repo(){
     dpkg -i percona-release_latest.generic_all.deb
     rm -f percona-release_latest.generic_all.deb
     percona-release disable all
-    percona-release enable ppg-${PG_VERSION} testing
+    percona-release enable ppg-${PG_VERSION} laboratory
     return
 }
 

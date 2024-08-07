f6015e83dfac35c74f4c98c16a48e202b44c9cdf
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-08-07 15:07:19 +0500

    [PG-867]: Fixes llvm-devel version issue on RHEL 8

--- postgres/ppg_builder.sh
+++ postgres/ppg_builder.sh
@@ -196,7 +196,8 @@ install_deps() {
         if [ x"$RHEL" = x8 ];
         then
             clang_version=$(yum list --showduplicates clang-devel | grep "17.0" | grep clang | awk '{print $2}' | head -n 1)
-            yum install -y clang-devel-${clang_version} clang-${clang_version} llvm-devel-${clang_version}
+            llvm_version=$(yum list --showduplicates llvm-devel | grep "17.0" | grep llvm | awk '{print $2}' | head -n 1)
+            yum install -y clang-devel-${clang_version} clang-${clang_version} llvm-devel-${llvm_version}
             dnf module -y disable llvm-toolset
         else
             yum install -y clang-devel clang llvm-devel

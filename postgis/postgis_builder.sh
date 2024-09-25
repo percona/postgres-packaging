3a5885b28a5a3ed94e2134c8fca3678ae72fbf6f
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-09-25 13:05:03 +0500

    [PG-1060]:Updates postgis pdf.

--- postgis/postgis_builder.sh
+++ postgis/postgis_builder.sh
@@ -386,8 +386,8 @@ build_srpm(){
     #
     cp -av rpm/* rpmbuild/SOURCES
     cd rpmbuild/SOURCES
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PPG_VERSION}/postgis/postgis-3.3.6.pdf
-    #wget --no-check-certificate https://download.osgeo.org/postgis/docs/postgis-3.3.6.pdf
+    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PPG_VERSION}/postgis/postgis-3.3.7.pdf
+    #wget --no-check-certificate https://download.osgeo.org/postgis/docs/postgis-3.3.7.pdf
     #wget --no-check-certificate https://www.postgresql.org/files/documentation/pdf/12/postgresql-12-A4.pdf
     cd ../../
     cp -av rpmbuild/SOURCES/percona-postgis33.spec rpmbuild/SPECS

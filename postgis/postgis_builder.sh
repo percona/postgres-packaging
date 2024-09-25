f586a34790e6f05fae25314e61a16a9b69a89087
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-09-25 13:21:30 +0500

    [PG-1060]: Fixes telemetry repo issue.

--- postgis/postgis_builder.sh
+++ postgis/postgis_builder.sh
@@ -304,6 +304,7 @@ install_deps() {
       dpkg -i percona-release_1.0-27.generic_all.deb
       percona-release enable-only tools testing
       percona-release enable-only ppg-${PPG_VERSION} testing
+      percona-release enable telemetry testing
       apt-get update
       if [ "x${DEBIAN}" = "xbionic" ]; then
         INSTALL_LIST="bison build-essential debconf debhelper devscripts dh-exec dpkg-dev flex gcc git cmake vim wget dctrl-tools docbook docbook-xsl imagemagick libcunit1-dev libgdal-dev libgeos-dev libjson-c-dev libpcre2-dev libproj-dev libprotobuf-c-dev libcgal-dev libxml2-dev pkg-config po-debconf percona-postgresql-all percona-postgresql-common percona-postgresql-server-dev-all percona-postgresql-${PG_MAJOR_VERSION} protobuf-c-compiler rdfind xsltproc"

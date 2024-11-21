de28fb1c304ed7f61e36392f4f499d90fa55b1c4
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-11-21 20:37:16 +0500

    Fixes llvm-devel package installation issue.

--- pg_repack/pgrepack_builder.sh
+++ pg_repack/pgrepack_builder.sh
@@ -215,10 +215,11 @@ install_deps() {
 	dnf config-manager --enable ol${RHEL}_codeready_builder
         if [ x"$RHEL" = x8 ]; then
 		clang_version=$(yum list --showduplicates clang-devel | grep "17.0" | grep clang | awk '{print $2}' | head -n 1)
-                yum install -y clang-devel-${clang_version} clang-${clang_version}
+                llvm_version=$(yum list --showduplicates llvm-devel | grep "17.0" | grep llvm | awk '{print $2}' | head -n 1)
+                yum install -y clang-devel-${clang_version} clang-${clang_version} llvm-devel-${llvm_version}
                 dnf module -y disable llvm-toolset
 
-                INSTALL_LIST="percona-postgresql${PG_MAJOR_VERSION} python3-devel perl-generators bison e2fsprogs-devel flex gettext git glibc-devel krb5-devel libicu-devel libselinux-devel libuuid-devel libxml2-devel libxslt-devel llvm-devel openldap-devel openssl-devel pam-devel patch perl perl-ExtUtils-MakeMaker perl-ExtUtils-Embed readline-devel percona-postgresql${PG_MAJOR_VERSION}-devel percona-postgresql${PG_MAJOR_VERSION}-server rpm-build rpmdevtools selinux-policy systemd systemd-devel systemtap-sdt-devel tcl-devel vim wget zlib-devel libzstd-devel lz4-devel"
+                INSTALL_LIST="percona-postgresql${PG_MAJOR_VERSION} python3-devel perl-generators bison e2fsprogs-devel flex gettext git glibc-devel krb5-devel libicu-devel libselinux-devel libuuid-devel libxml2-devel libxslt-devel openldap-devel openssl-devel pam-devel patch perl perl-ExtUtils-MakeMaker perl-ExtUtils-Embed readline-devel percona-postgresql${PG_MAJOR_VERSION}-devel percona-postgresql${PG_MAJOR_VERSION}-server rpm-build rpmdevtools selinux-policy systemd systemd-devel systemtap-sdt-devel tcl-devel vim wget zlib-devel libzstd-devel lz4-devel"
         	yum -y install ${INSTALL_LIST}
         	yum -y install binutils gcc gcc-c++
 	else
--- timescaledb/timescaledb_builder.sh
+++ timescaledb/timescaledb_builder.sh
@@ -214,11 +214,12 @@ install_deps() {
 
           if [ x"$RHEL" = x8 ];
           then
-              clang_version=$(yum list --showduplicates clang-devel | grep "17.0" | grep clang | awk '{print $2}' | head -n 1)
-              yum install -y clang-${clang_version}
+	      clang_version=$(yum list --showduplicates clang-devel | grep "17.0" | grep clang | awk '{print $2}' | head -n 1)
+              llvm_version=$(yum list --showduplicates llvm-devel | grep "17.0" | grep llvm | awk '{print $2}' | head -n 1)
+              yum install -y clang-devel-${clang_version} clang-${clang_version} llvm-devel-${llvm_version}
               dnf module -y disable llvm-toolset
           else
-              yum install -y clang
+              yum install -y clang llvm-devel
           fi
           yum -y install cmake
       else

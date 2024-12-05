db8d2584fbbb0a153a53d17db9b1d7e54527252e
Muhammad Aqeel <muhammad.aqeel@percona.com>
2024-12-05 17:27:11 +0500

    [PKG-314]: Pulls latest pg_tde source.

--- postgres/ppg_builder.sh
+++ postgres/ppg_builder.sh
@@ -106,6 +106,10 @@ get_sources(){
         git clean -xdf
         git checkout "$BRANCH"
         git submodule update --init --recursive
+        cd contrib/pg_tde
+        git fetch
+        git reset --hard origin/main
+        cd -
 	sed -i 's:enable_tap_tests=no:enable_tap_tests=yes:' configure
     fi
     REVISION=$(git rev-parse --short HEAD)

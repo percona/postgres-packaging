d8171e71f1f799c5e4a9e7ad27f9d56d49320c79
Manika Singhal <manika.singhal@percona.com>
2025-07-17 11:39:59 +0530

    update github url

--- postgres/ppg_builder.sh
+++ postgres/ppg_builder.sh
@@ -518,7 +518,7 @@ RPM_RELEASE=1
 DEB_RELEASE=1
 REVISION=0
 BRANCH="REL_15_13"
-REPO="git://git.postgresql.org/git/postgresql.git"
+REPO="https://git.postgresql.org/git/postgresql.git"
 PRODUCT=percona-postgresql
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"

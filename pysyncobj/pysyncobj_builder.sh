afef63ee8acf8b8874a67e49b2f67e18af2ea935
Manika Singhal <manika.singhal@percona.com>
2025-07-21 22:03:52 +0530

    fix

--- pysyncobj/pysyncobj_builder.sh
+++ pysyncobj/pysyncobj_builder.sh
@@ -169,7 +169,7 @@ install_deps() {
       if [ x"$RHEL" = x8 ]; then
           switch_to_vault_repo
       fi
-      yum -y install wget epel-release
+      yum -y install epel-release wget
       yum clean all
       RHEL=$(rpm --eval %rhel)
       if [ ${RHEL} = 7 ]; then

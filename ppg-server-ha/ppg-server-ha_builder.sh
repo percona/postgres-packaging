ed40e4e7ba965f5b580db9fc25de948a01f0adbf
Manika Singhal <manika.singhal@percona.com>
2025-07-22 12:09:54 +0530

    fix

--- ppg-server-ha/ppg-server-ha_builder.sh
+++ ppg-server-ha/ppg-server-ha_builder.sh
@@ -77,9 +77,6 @@ check_workdir(){
 
 add_percona_yum_repo(){
     yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
-    wget https://raw.githubusercontent.com/percona/percona-repositories/release-1.0-28/scripts/percona-release.sh
-    mv percona-release.sh /usr/bin/percona-release
-    chmod 777 /usr/bin/percona-release
     percona-release disable all
     percona-release enable ppg-${PG_VERSION} testing
     return

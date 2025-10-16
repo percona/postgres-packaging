aa176b4cdd307fa899e1381567626b3b71da1940
Manika Singhal <manika.singhal@percona.com>
2025-10-16 10:32:07 +0530

    Packaging improvements for PG14
    1. PG-1838: A separate file (versions.sh) which contains the versions and URLs required for PG and all other components.
    
    2. PG-1839: A designated separate file (common-functions.sh) for all the common functions used in every build script.
    
    3. A designated separate file (install-deps.sh) just to install all the dependencies required to build PG or any other component.

--- wal2json/wal2json_builder.sh
+++ wal2json/wal2json_builder.sh
@@ -1,93 +1,9 @@
 #!/usr/bin/env bash
-
-shell_quote_string() {
-  echo "$1" | sed -e 's,\([^a-zA-Z0-9/_.=-]\),\\\1,g'
-}
-
-usage () {
-    cat <<EOF
-Usage: $0 [OPTIONS]
-    The following options may be given :
-        --builddir=DIR      Absolute path to the dir where all actions will be performed
-        --get_sources       Source will be downloaded from github
-        --build_src_rpm     If it is set - src rpm will be built
-        --build_src_deb  If it is set - source deb package will be built
-        --build_rpm         If it is set - rpm will be built
-        --build_deb         If it is set - deb will be built
-        --install_deps      Install build dependencies(root privilages are required)
-        --branch            Branch for build
-        --repo              Repo for build
-        --help) usage ;;
-Example $0 --builddir=/tmp/BUILD --get_sources=1 --build_src_rpm=1 --build_rpm=1
-EOF
-        exit 1
-}
-
-append_arg_to_args () {
-  args="$args "$(shell_quote_string "$1")
-}
-
-parse_arguments() {
-    pick_args=
-    if test "$1" = PICK-ARGS-FROM-ARGV
-    then
-        pick_args=1
-        shift
-    fi
-
-    for arg do
-        val=$(echo "$arg" | sed -e 's;^--[^=]*=;;')
-        case "$arg" in
-            --builddir=*) WORKDIR="$val" ;;
-            --build_src_rpm=*) SRPM="$val" ;;
-            --build_src_deb=*) SDEB="$val" ;;
-            --build_rpm=*) RPM="$val" ;;
-            --build_deb=*) DEB="$val" ;;
-            --get_sources=*) SOURCE="$val" ;;
-            --branch=*) BRANCH="$val" ;;
-            --repo=*) REPO="$val" ;;
-            --install_deps=*) INSTALL="$val" ;;
-            --help) usage ;;
-            *)
-              if test -n "$pick_args"
-              then
-                  append_arg_to_args "$arg"
-              fi
-              ;;
-        esac
-    done
-}
-
-check_workdir(){
-    if [ "x$WORKDIR" = "x$CURDIR" ]
-    then
-        echo >&2 "Current directory cannot be used for building!"
-        exit 1
-    else
-        if ! test -d "$WORKDIR"
-        then
-            echo >&2 "$WORKDIR is not a directory."
-            exit 1
-        fi
-    fi
-    return
-}
-
-add_percona_yum_repo(){
-    yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
-    percona-release disable all
-    percona-release enable ppg-${PG_VERSION} testing
-    return
-}
-
-add_percona_apt_repo(){
-    wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
-    dpkg -i percona-release_latest.generic_all.deb
-    rm -f percona-release_latest.generic_all.deb
-    percona-release disable all
-    percona-release enable ppg-${PG_VERSION} testing
-    return
-}
+set -x
+# Versions and other variables
+source versions.sh "wal2json"
+# Common functions
+source common-functions.sh
 
 get_sources(){
     cd "${WORKDIR}"
@@ -96,171 +12,67 @@ get_sources(){
         echo "Sources will not be downloaded"
         return 0
     fi
-    PRODUCT=percona-wal2json
-    echo "PRODUCT=${PRODUCT}" > wal2json.properties
 
-    PRODUCT_FULL=${PRODUCT}-${VERSION}
-    echo "PRODUCT_FULL=${PRODUCT_FULL}" >> wal2json.properties
+    echo "PRODUCT=${WAL2JSON_PRODUCT}" > wal2json.properties
+    echo "PRODUCT_FULL=${WAL2JSON_PRODUCT_FULL}" >> wal2json.properties
     echo "VERSION=${PSM_VER}" >> wal2json.properties
     echo "BUILD_NUMBER=${BUILD_NUMBER}" >> wal2json.properties
     echo "BUILD_ID=${BUILD_ID}" >> wal2json.properties
-    git clone "$REPO" ${PRODUCT_FULL}
+    git clone "$WAL2JSON_SRC_REPO" ${WAL2JSON_PRODUCT_FULL}
     retval=$?
     if [ $retval != 0 ]
     then
         echo "There were some issues during repo cloning from github. Please retry one more time"
         exit 1
     fi
-    cd ${PRODUCT_FULL}
-    if [ ! -z "$BRANCH" ]
+    cd ${WAL2JSON_PRODUCT_FULL}
+    if [ ! -z "$WAL2JSON_SRC_BRANCH" ]
     then
         git reset --hard
         git clean -xdf
-        git checkout "$BRANCH"
+        git checkout "$WAL2JSON_SRC_BRANCH"
         git submodule update --init
     fi
     REVISION=$(git rev-parse --short HEAD)
     echo "REVISION=${REVISION}" >> ${WORKDIR}/wal2json.properties
     rm -fr debian rpm
 
-    git clone https://salsa.debian.org/postgresql/wal2json.git deb_packaging
+    git clone ${WAL2JSON_SRC_REPO_DEB} deb_packaging
     mv deb_packaging/debian ./
     cd debian/
     for file in $(ls | grep ^wal2json | grep -v wal2json.conf); do
         mv $file "percona-$file"
     done
     rm -rf changelog
-    echo "percona-wal2json (${VERSION}-${RELEASE}) unstable; urgency=low" >> changelog
+    echo "percona-wal2json (${WAL2JSON_VERSION}-${WAL2JSON_RELEASE}) unstable; urgency=low" >> changelog
     echo "  * Initial Release." >> changelog
     echo " -- EvgeniyPatlan <evgeniy.patlan@percona.com> $(date -R)" >> changelog
     rm -f control rules
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/wal2json/control
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/wal2json/control.in
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/wal2json/rules
-    echo 14 > pgversions
+    wget ${PKG_RAW_URL}/wal2json/control
+    wget ${PKG_RAW_URL}/wal2json/control.in
+    wget ${PKG_RAW_URL}/wal2json/rules
+    echo ${PG_MAJOR} > pgversions
     echo 9 > compat
     cd ../
     rm -rf deb_packaging
     mkdir rpm
     cd rpm
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/wal2json/percona-wal2json.spec
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/wal2json/wal2json-pg14-makefile-pgxs.patch
+    wget ${PKG_RAW_URL}/wal2json/percona-wal2json.spec
+    wget ${PKG_RAW_URL}/wal2json/wal2json-pg${PG_MAJOR}-makefile-pgxs.patch
     cd ${WORKDIR}
     #
     source wal2json.properties
     #
 
-    tar --owner=0 --group=0 --exclude=.* -czf ${PRODUCT_FULL}.tar.gz ${PRODUCT_FULL}
+    tar --owner=0 --group=0 --exclude=.* -czf ${WAL2JSON_PRODUCT_FULL}.tar.gz ${WAL2JSON_PRODUCT_FULL}
     DATE_TIMESTAMP=$(date +%F_%H-%M-%S)
-    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}/${PRODUCT_FULL}/${PSM_BRANCH}/${REVISION}/${DATE_TIMESTAMP}/${BUILD_ID}" >> wal2json.properties
+    echo "UPLOAD=UPLOAD/experimental/BUILDS/${WAL2JSON_PRODUCT_FULL}/${WAL2JSON_PRODUCT_FULL}/${PSM_BRANCH}/${REVISION}/${DATE_TIMESTAMP}/${BUILD_ID}" >> wal2json.properties
     mkdir $WORKDIR/source_tarball
     mkdir $CURDIR/source_tarball
-    cp ${PRODUCT_FULL}.tar.gz $WORKDIR/source_tarball
-    cp ${PRODUCT_FULL}.tar.gz $CURDIR/source_tarball
+    cp ${WAL2JSON_PRODUCT_FULL}.tar.gz $WORKDIR/source_tarball
+    cp ${WAL2JSON_PRODUCT_FULL}.tar.gz $CURDIR/source_tarball
     cd $CURDIR
     rm -rf percona-wal2json*
-    return
-}
-
-get_system(){
-    if [ -f /etc/redhat-release ]; then
-        RHEL=$(rpm --eval %rhel)
-        ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-        OS_NAME="el$RHEL"
-        OS="rpm"
-    else
-        ARCH=$(uname -m)
-        OS_NAME="$(lsb_release -sc)"
-        OS="deb"
-    fi
-    return
-}
-switch_to_vault_repo() {
-    sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
-}
-
-install_deps() {
-    if [ $INSTALL = 0 ]
-    then
-        echo "Dependencies will not be installed"
-        return;
-    fi
-    if [ $( id -u ) -ne 0 ]
-    then
-        echo "It is not possible to instal dependencies. Please run as root"
-        exit 1
-    fi
-    CURPLACE=$(pwd)
-
-    if [ "x$OS" = "xrpm" ]; then
-      RHEL=$(rpm --eval %rhel)
-      if [ x"$RHEL" = x8 ]; then
-          switch_to_vault_repo
-      fi
-      yum -y install wget
-      add_percona_yum_repo
-      yum clean all
-      if [[ "${RHEL}" -eq 10 ]]; then
-        yum install oracle-epel-release-el10
-      else
-        yum -y install epel-release
-      fi
-      if [ ${RHEL} -gt 7 ]; then
-          dnf -y module disable postgresql
-          dnf config-manager --set-enabled ol${RHEL}_codeready_builder
-          dnf clean all
-          rm -r /var/cache/dnf
-          dnf -y upgrade
-	  switch_to_vault_repo
-
-          yum -y install clang-devel clang llvm-devel perl lz4-libs c-ares-devel
-      else
-        until yum -y install centos-release-scl; do
-            echo "waiting"
-            sleep 1
-        done
-        yum -y install llvm-toolset-7-clang llvm5.0-devtoolset
-        source /opt/rh/devtoolset-7/enable
-        source /opt/rh/llvm-toolset-7/enable
-      fi
-      INSTALL_LIST="pandoc libtool libevent-devel python3-psycopg2 openssl-devel pam-devel percona-postgresql14-devel git rpmdevtools systemd systemd-devel wget libxml2-devel perl perl-DBD-Pg perl-Digest-SHA perl-IO-Socket-SSL perl-JSON-PP zlib-devel gcc make autoconf perl-ExtUtils-Embed"
-      yum -y install ${INSTALL_LIST}
-      yum -y install lz4 || true
-
-    else
-      export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-      apt-get update || true
-      apt-get -y install lsb-release wget gnupg2 curl
-      export DEBIAN=$(lsb_release -sc)
-      add_percona_apt_repo
-      apt-get update || true
-      INSTALL_LIST="build-essential pkg-config liblz4-dev debconf debhelper devscripts dh-exec git wget libxml-checker-perl libxml-libxml-perl libio-socket-ssl-perl libperl-dev libssl-dev libxml2-dev txt2man zlib1g-dev libpq-dev percona-postgresql-14 percona-postgresql-common percona-postgresql-server-dev-all percona-postgresql-all libbz2-dev libzstd-dev libevent-dev libssl-dev libc-ares-dev pandoc pkg-config"
-      until DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated install ${INSTALL_LIST}; do
-        sleep 1
-        echo "waiting"
-      done
-      DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated install libpam0g-dev || DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated install libpam-dev
-    fi
-    return;
-}
-
-get_tar(){
-    TARBALL=$1
-    TARFILE=$(basename $(find $WORKDIR/$TARBALL -name 'percona-wal2json*.tar.gz' | sort | tail -n1))
-    if [ -z $TARFILE ]
-    then
-        TARFILE=$(basename $(find $CURDIR/$TARBALL -name 'percona-wal2json*.tar.gz' | sort | tail -n1))
-        if [ -z $TARFILE ]
-        then
-            echo "There is no $TARBALL for build"
-            exit 1
-        else
-            cp $CURDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
-        fi
-    else
-        cp $WORKDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
-    fi
     return
 }
 
@@ -296,7 +108,7 @@ build_srpm(){
         exit 1
     fi
     cd $WORKDIR
-    get_tar "source_tarball"
+    get_tar "source_tarball" "percona-wal2json"
     rm -fr rpmbuild
     ls | grep -v tar.gz | xargs rm -rf
     TARFILE=$(find . -name 'percona-wal2json*.tar.gz' | sort | tail -n1)
@@ -309,8 +121,8 @@ build_srpm(){
     cp -av rpm/percona-wal2json.spec rpmbuild/SPECS
     #
     mv -fv ${TARFILE} ${WORKDIR}/rpmbuild/SOURCES
-    rpmbuild -bs --define "_topdir ${WORKDIR}/rpmbuild" --define "pginstdir /usr/pgsql-14" --define "dist .generic" \
-        --define "version ${VERSION}" rpmbuild/SPECS/percona-wal2json.spec
+    rpmbuild -bs --define "_topdir ${WORKDIR}/rpmbuild" --define "pginstdir /usr/pgsql-$PG_MAJOR" --define "dist .generic" \
+        --define "version ${WAL2JSON_VERSION}" rpmbuild/SPECS/percona-wal2json.spec
     mkdir -p ${WORKDIR}/srpm
     mkdir -p ${CURDIR}/srpm
     cp rpmbuild/SRPMS/*.src.rpm ${CURDIR}/srpm
@@ -358,12 +170,12 @@ build_rpm(){
         source /opt/rh/devtoolset-7/enable
         source /opt/rh/llvm-toolset-7/enable
     fi
-    export LIBPQ_DIR=/usr/pgsql-14/
-    export LIBRARY_PATH=/usr/pgsql-14/lib/:/usr/pgsql-14/include/
+    export LIBPQ_DIR=/usr/pgsql-${PG_MAJOR}/
+    export LIBRARY_PATH=/usr/pgsql-${PG_MAJOR}/lib/:/usr/pgsql-${PG_MAJOR}/include/
     if [[ "${RHEL}" -eq 10 ]]; then
         export QA_RPATHS=0x0002
     fi
-    rpmbuild --define "_topdir ${WORKDIR}/rpmbuild" --define "pginstdir /usr/pgsql-14" --define "dist .$OS_NAME" --define "version ${VERSION}" --rebuild rpmbuild/SRPMS/$SRC_RPM
+    rpmbuild --define "_topdir ${WORKDIR}/rpmbuild" --define "pginstdir /usr/pgsql-$PG_MAJOR" --define "dist .$OS_NAME" --define "version ${WAL2JSON_VERSION}" --rebuild rpmbuild/SRPMS/$SRC_RPM
 
     return_code=$?
     if [ $return_code != 0 ]; then
@@ -387,7 +199,7 @@ build_source_deb(){
         exit 1
     fi
     rm -rf percona-wal2json*
-    get_tar "source_tarball"
+    get_tar "source_tarball" "percona-wal2json"
     rm -f *.dsc *.orig.tar.gz *.debian.tar.gz *.changes
     #
     TARFILE=$(basename $(find . -name 'percona-*wal2json*.tar.gz' | sort | tail -n1))
@@ -397,10 +209,10 @@ build_source_deb(){
     BUILDDIR=${TARFILE%.tar.gz}
     #
     
-    mv ${TARFILE} ${PRODUCT}_${VERSION}.orig.tar.gz
+    mv ${TARFILE} ${WAL2JSON_PRODUCT}_${WAL2JSON_VERSION}.orig.tar.gz
     cd ${BUILDDIR}
   
-    dch -D unstable --force-distribution -v "${VERSION}-${RELEASE}" "Update to new wal2json version ${VERSION}"
+    dch -D unstable --force-distribution -v "${WAL2JSON_VERSION}-${WAL2JSON_RELEASE}" "Update to new wal2json version ${WAL2JSON_VERSION}"
     dpkg-buildpackage -S
     cd ../
     mkdir -p $WORKDIR/source_deb
@@ -444,8 +256,8 @@ build_deb(){
     #
     dpkg-source -x ${DSC}
     #
-    cd ${PRODUCT}-${VERSION}
-    dch -m -D "${DEBIAN}" --force-distribution -v "1:${VERSION}-${RELEASE}.${DEBIAN}" 'Update distribution'
+    cd ${WAL2JSON_PRODUCT_FULL}
+    dch -m -D "${DEBIAN}" --force-distribution -v "1:${WAL2JSON_VERSION}-${WAL2JSON_RELEASE}.${DEBIAN}" 'Update distribution'
     unset $(locale|cut -d= -f1)
     dpkg-buildpackage -rfakeroot -us -uc -b
     mkdir -p $CURDIR/deb
@@ -472,22 +284,18 @@ OS_NAME=
 ARCH=
 OS=
 INSTALL=0
-RPM_RELEASE=1
-DEB_RELEASE=1
 REVISION=0
-BRANCH="wal2json_2_6"
-REPO="https://github.com/eulerto/wal2json.git"
-PRODUCT=percona-wal2json
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='2.6'
-RELEASE='2'
-PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
-PG_VERSION=14.19
 
 check_workdir
 get_system
-install_deps
+#install_deps
+if [ $INSTALL = 0 ]; then
+    echo "Dependencies will not be installed"
+else
+    source install-deps.sh "wal2json"
+fi
 get_sources
 build_srpm
 build_source_deb

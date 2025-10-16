b4a15fc2b26c76854b991bccad4f5a34de69c76d
Manika Singhal <manika.singhal@percona.com>
2025-10-16 10:30:56 +0530

    Packaging improvements for PG13
    1. PG-1838: A separate file (versions.sh) which contains the versions and URLs required for PG and all other components.
    
    2. PG-1839: A designated separate file (common-functions.sh) for all the common functions used in every build script.
    
    3. A designated separate file (install-deps.sh) just to install all the dependencies required to build PG or any other component.

--- ydiff/ydiff_builder.sh
+++ ydiff/ydiff_builder.sh
@@ -1,82 +1,9 @@
 #!/usr/bin/env bash
-
-shell_quote_string() {
-  echo "$1" | sed -e 's,\([^a-zA-Z0-9/_.=-]\),\\\1,g'
-}
-
-usage () {
-    cat <<EOF
-Usage: $0 [OPTIONS]
-    The following options may be given :
-        --builddir=DIR      Absolute path to the dir where all actions will be performed
-        --get_sources       Source will be downloaded from github
-        --build_src_rpm     If it is set - src rpm will be built
-        --build_src_deb  If it is set - source deb package will be built
-        --build_rpm         If it is set - rpm will be built
-        --build_deb         If it is set - deb will be built
-        --install_deps      Install build dependencies(root privilages are required)
-        --branch            Branch for build
-        --repo              Repo for build
-        --help) usage ;;
-Example $0 --builddir=/tmp/BUILD --get_sources=1 --build_src_rpm=1 --build_rpm=1
-EOF
-        exit 1
-}
-
-append_arg_to_args () {
-  args="$args "$(shell_quote_string "$1")
-}
-
-parse_arguments() {
-    pick_args=
-    if test "$1" = PICK-ARGS-FROM-ARGV
-    then
-        pick_args=1
-        shift
-    fi
-
-    for arg do
-        val=$(echo "$arg" | sed -e 's;^--[^=]*=;;')
-        case "$arg" in
-            --builddir=*) WORKDIR="$val" ;;
-            --build_src_rpm=*) SRPM="$val" ;;
-            --build_src_deb=*) SDEB="$val" ;;
-            --build_rpm=*) RPM="$val" ;;
-            --build_deb=*) DEB="$val" ;;
-            --get_sources=*) SOURCE="$val" ;;
-            --branch=*) BRANCH="$val" ;;
-            --repo=*) REPO="$val" ;;
-            --install_deps=*) INSTALL="$val" ;;
-            --help) usage ;;
-            *)
-              if test -n "$pick_args"
-              then
-                  append_arg_to_args "$arg"
-              fi
-              ;;
-        esac
-    done
-}
-
-check_workdir(){
-    if [ "x$WORKDIR" = "x$CURDIR" ]
-    then
-        echo >&2 "Current directory cannot be used for building!"
-        exit 1
-    else
-        if ! test -d "$WORKDIR"
-        then
-            echo >&2 "$WORKDIR is not a directory."
-            exit 1
-        fi
-    fi
-    return
-}
-
-switch_to_vault_repo() {
-    sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
-}
+set -x
+# Versions and other variables
+source versions.sh "ydiff"
+# Common functions
+source common-functions.sh
 
 get_sources(){
     cd "${WORKDIR}"
@@ -85,160 +12,62 @@ get_sources(){
         echo "Sources will not be downloaded"
         return 0
     fi
-    PRODUCT=python3-ydiff
-    echo "PRODUCT=${PRODUCT}" > ydiff.properties
-    GIT_USER=$(echo ${REPO} | awk -F'/' '{print $4}')
 
-    PRODUCT_FULL=${PRODUCT}-${VERSION}
-    echo "PRODUCT_FULL=${PRODUCT_FULL}" >> ydiff.properties
-    echo "VERSION=${VERSION}" >> ydiff.properties
+    echo "PRODUCT=${YDIFF_PRODUCT}" > ydiff.properties
+    GIT_USER=$(echo ${YDIFF_SRC_REPO} | awk -F'/' '{print $4}')
+    echo "PRODUCT_FULL=${YDIFF_PRODUCT_FULL}" >> ydiff.properties
+    echo "VERSION=${YDIFF_VERSION}" >> ydiff.properties
     echo "BUILD_NUMBER=${BUILD_NUMBER}" >> ydiff.properties
     echo "BUILD_ID=${BUILD_ID}" >> ydiff.properties
-    git clone "$REPO" ydiff-${VERSION}
+    git clone "$YDIFF_SRC_REPO" ydiff-${YDIFF_VERSION}
     retval=$?
     if [ $retval != 0 ]
     then
         echo "There were some issues during repo cloning from github. Please retry one more time"
         exit 1
     fi
-    cd ydiff-${VERSION}
-    if [ ! -z "$BRANCH" ]
+    cd ydiff-${YDIFF_VERSION}
+    if [ ! -z "$YDIFF_SRC_BRANCH" ]
     then
         git reset --hard
         git clean -xdf
-        git checkout "$BRANCH"
+        git checkout "$YDIFF_SRC_BRANCH"
     fi
     REVISION=$(git rev-parse --short HEAD)
     echo "REVISION=${REVISION}" >> ${WORKDIR}/ydiff.properties
     rm -fr debian rpm
     mkdir -p debian/source
     cd debian
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/rules
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/changelog
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/compat
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/control
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/copyright
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/docs
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/watch
+    wget ${PKG_RAW_URL}/ydiff/debian/rules
+    wget ${PKG_RAW_URL}/ydiff/debian/changelog
+    wget ${PKG_RAW_URL}/ydiff/debian/compat
+    wget ${PKG_RAW_URL}/ydiff/debian/control
+    wget ${PKG_RAW_URL}/ydiff/debian/copyright
+    wget ${PKG_RAW_URL}/ydiff/debian/docs
+    wget ${PKG_RAW_URL}/ydiff/debian/watch
     cd source
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/debian/source/format
+    wget ${PKG_RAW_URL}/ydiff/debian/source/format
     cd ../../
 
     export DEBIAN=$(lsb_release -sc)
     mkdir rpm
     cd rpm
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/ydiff/rpm/ydiff.spec
+    wget ${PKG_RAW_URL}/ydiff/rpm/ydiff.spec
     cd ../
     cd ${WORKDIR}
-    #
-    source ydiff.properties
-    #
 
-    tar --owner=0 --group=0 --exclude=.* -czf ydiff-${VERSION}.tar.gz ydiff-${VERSION}
+    source ydiff.properties
+
+
+    tar --owner=0 --group=0 --exclude=.* -czf ydiff-${YDIFF_VERSION}.tar.gz ydiff-${YDIFF_VERSION}
     DATE_TIMESTAMP=$(date +%F_%H-%M-%S)
-    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}/${PRODUCT_FULL}/${BRANCH}/${REVISION}/${DATE_TIMESTAMP}/${BUILD_ID}" >> ydiff.properties
+    echo "UPLOAD=UPLOAD/experimental/BUILDS/${YDIFF_PRODUCT}/${YDIFF_PRODUCT_FULL}/${YDIFF_SRC_BRANCH}/${REVISION}/${DATE_TIMESTAMP}/${BUILD_ID}" >> ydiff.properties
     mkdir $WORKDIR/source_tarball
     mkdir $CURDIR/source_tarball
-    cp ydiff-${VERSION}.tar.gz $WORKDIR/source_tarball
-    cp ydiff-${VERSION}.tar.gz $CURDIR/source_tarball
+    cp ydiff-${YDIFF_VERSION}.tar.gz $WORKDIR/source_tarball
+    cp ydiff-${YDIFF_VERSION}.tar.gz $CURDIR/source_tarball
     cd $CURDIR
     rm -rf python3-ydiff*
-    return
-}
-
-get_system(){
-    if [ -f /etc/redhat-release ]; then
-        RHEL=$(rpm --eval %rhel)
-        ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-        OS_NAME="el$RHEL"
-        OS="rpm"
-    else
-        ARCH=$(uname -m)
-        OS_NAME="$(lsb_release -sc)"
-        OS="deb"
-    fi
-    return
-}
-
-install_deps() {
-    if [ $INSTALL = 0 ]
-    then
-        echo "Dependencies will not be installed"
-        return;
-    fi
-    if [ $( id -u ) -ne 0 ]
-    then
-        echo "It is not possible to instal dependencies. Please run as root"
-        exit 1
-    fi
-    CURPLACE=$(pwd)
-
-    if [ "x$OS" = "xrpm" ]; then
-      if [ x"$RHEL" = x8 ]; then
-          switch_to_vault_repo
-      fi
-      yum -y install wget
-      yum clean all
-      if [[ "${RHEL}" -eq 10 ]]; then
-        yum install oracle-epel-release-el10
-      else
-        yum -y install epel-release
-      fi
-      RHEL=$(rpm --eval %rhel)
-      if [ ${RHEL} -gt 7 ]; then
-          yum config-manager --set-enabled PowerTools || yum config-manager --set-enabled powertools || true
-      fi
-      if [ ${RHEL} = 7 ]; then
-          INSTALL_LIST="git wget rpm-build python3-devel rpmdevtools"
-          yum -y install ${INSTALL_LIST}
-      else
-          dnf config-manager --set-enabled ol${RHEL}_codeready_builder
-          dnf clean all
-          rm -r /var/cache/dnf
-          dnf -y upgrade
-          INSTALL_LIST="git wget rpm-build python3-devel python3-setuptools rpmdevtools"
-          yum -y install ${INSTALL_LIST}
-      fi
-    else
-      apt-get update || true
-      apt-get -y install lsb-release wget curl gnupg2
-      export DEBIAN=$(lsb_release -sc)
-      export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-      until apt-get -y install gnupg2; do
-          sleep 3
-	  echo "WAITING"
-      done
-      apt-get update || true
-
-      INSTALL_LIST="build-essential debconf debhelper devscripts dh-exec git wget fakeroot devscripts python3-psycopg2 python3-setuptools libyaml-dev python3-virtualenv ruby ruby-dev rubygems curl golang dh-python libjs-mathjax pyflakes3 python3-dateutil python3-dnspython python3-etcd  python3-flake8 python3-kazoo python3-mccabe python3-mock python3-prettytable python3-psutil python3-pycodestyle python3-pytest python3-pytest-cov python3-sphinx python3-sphinx-rtd-theme python3-tz python3-tzlocal sphinx-common python3-click python3-doc python3-all "
-      if [ "x${DEBIAN}" = "xtrixie" ]; then
-        INSTALL_LIST+="python3-dev dh-virtualenv python3-boto3"  
-      elif [ "x${DEBIAN}" != "xfocal" ]; then
-        INSTALL_LIST+="python3-dev dh-virtualenv python3-boto"
-      else
-        INSTALL_LIST+="python2-dev python3-boto"
-      fi
-      DEBIAN_FRONTEND=noninteractive apt-get -y install ${INSTALL_LIST}
-    fi
-    return;
-}
-
-get_tar(){
-    TARBALL=$1
-    TARFILE=$(basename $(find $WORKDIR/$TARBALL -name 'ydiff*.tar.gz' | sort | tail -n1))
-    if [ -z $TARFILE ]
-    then
-        TARFILE=$(basename $(find $CURDIR/$TARBALL -name 'ydiff*.tar.gz' | sort | tail -n1))
-        if [ -z $TARFILE ]
-        then
-            echo "There is no $TARBALL for build"
-            exit 1
-        else
-            cp $CURDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
-        fi
-    else
-        cp $WORKDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
-    fi
     return
 }
 
@@ -274,22 +103,22 @@ build_srpm(){
         exit 1
     fi
     cd $WORKDIR
-    get_tar "source_tarball"
+    get_tar "source_tarball" "ydiff"
     rm -fr rpmbuild
     ls | grep -v tar.gz | xargs rm -rf
     TARFILE=$(find . -name 'ydiff*.tar.gz' | sort | tail -n1)
     SRC_DIR=${TARFILE%.tar.gz}
-    #
+
     mkdir -vp rpmbuild/{SOURCES,SPECS,BUILD,SRPMS,RPMS}
     tar vxzf ${WORKDIR}/${TARFILE} --wildcards '*/rpm' --strip=1
-    #
+
     cp -av rpm/* rpmbuild/SOURCES
     cp -av rpm/ydiff.spec rpmbuild/SPECS
-    #
+
     mv -fv ${TARFILE} ${WORKDIR}/rpmbuild/SOURCES
     sed -i 's:.rhel7:%{dist}:' ${WORKDIR}/rpmbuild/SPECS/ydiff.spec
     rpmbuild -bs --define "_topdir ${WORKDIR}/rpmbuild" --define "dist .generic" \
-        --define "version ${VERSION}" rpmbuild/SPECS/ydiff.spec
+        --define "version ${YDIFF_VERSION}" rpmbuild/SPECS/ydiff.spec
     mkdir -p ${WORKDIR}/srpm
     mkdir -p ${CURDIR}/srpm
     cp rpmbuild/SRPMS/*.src.rpm ${CURDIR}/srpm
@@ -329,11 +158,11 @@ build_rpm(){
     cp $SRC_RPM rb/SRPMS/
 
     cd rb/SRPMS/
-    #
+
     cd $WORKDIR
     RHEL=$(rpm --eval %rhel)
     ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-    rpmbuild --define "_topdir ${WORKDIR}/rb" --define "dist .$OS_NAME" --define "version ${VERSION}" --rebuild rb/SRPMS/$SRC_RPM
+    rpmbuild --define "_topdir ${WORKDIR}/rb" --define "dist .$OS_NAME" --define "version ${YDIFF_VERSION}" --rebuild rb/SRPMS/$SRC_RPM
 
     return_code=$?
     if [ $return_code != 0 ]; then
@@ -357,21 +186,21 @@ build_source_deb(){
         exit 1
     fi
     rm -rf python3-ydiff*
-    get_tar "source_tarball"
+    get_tar "source_tarball" "ydiff"
     rm -f *.dsc *.orig.tar.gz *.debian.tar.gz *.changes
-    #
+
     TARFILE=$(basename $(find . -name 'ydiff*.tar.gz' | sort | tail -n1))
     DEBIAN=$(lsb_release -sc)
     ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
     tar zxf ${TARFILE}
-    rm -f ydiff-${VERSION}/.travis.yml
+    rm -f ydiff-${YDIFF_VERSION}/.travis.yml
     BUILDDIR=${TARFILE%.tar.gz}
-    #
+
     
-    mv ${TARFILE} ydiff_${VERSION}.orig.tar.gz
+    mv ${TARFILE} ydiff_${YDIFF_VERSION}.orig.tar.gz
     cd ${BUILDDIR}
 
-    dch -D unstable --force-distribution -v "${VERSION}-${RELEASE}" "Update to new ydiff version ${VERSION}"
+    dch -D unstable --force-distribution -v "${YDIFF_VERSION}-${YDIFF_RELEASE}" "Update to new ydiff version ${YDIFF_VERSION}"
     dpkg-buildpackage -S || true
     cd ../
     mkdir -p $WORKDIR/source_deb
@@ -403,20 +232,20 @@ build_deb(){
     done
     cd $WORKDIR
     rm -fv *.deb
-    #
+
     export DEBIAN=$(lsb_release -sc)
     export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-    #
+
     echo "DEBIAN=${DEBIAN}" >> ydiff.properties
     echo "ARCH=${ARCH}" >> ydiff.properties
 
-    #
+
     DSC=$(basename $(find . -name '*.dsc' | sort | tail -n1))
-    #
+
     dpkg-source -x ${DSC}
-    #
-    cd ydiff-${VERSION}
-    dch -m -D "${DEBIAN}" --force-distribution -v "1:${VERSION}-${RELEASE}.${DEBIAN}" 'Update distribution'
+
+    cd ydiff-${YDIFF_VERSION}
+    dch -m -D "${DEBIAN}" --force-distribution -v "1:${YDIFF_VERSION}-${YDIFF_RELEASE}.${DEBIAN}" 'Update distribution'
     unset $(locale|cut -d= -f1)
     dpkg-buildpackage -rfakeroot -us -uc -b
     mkdir -p $CURDIR/deb
@@ -443,23 +272,19 @@ OS_NAME=
 ARCH=
 OS=
 INSTALL=0
-RPM_RELEASE=1
-DEB_RELEASE=1
 REVISION=0
-BRANCH="1.2"
-REPO="https://github.com/ymattw/ydiff.git"
-PRODUCT=python3-ydiff
 DEBUG=0
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='1.2'
-RELEASE='2'
-PG_VERSION=13.22
-PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
-PG_MAJOR_VERSION=$(echo ${PG_VERSION} | cut -f1 -d'.')
+
 
 check_workdir
 get_system
-install_deps
+#install_deps
+if [ $INSTALL = 0 ]; then
+    echo "Dependencies will not be installed"
+else
+    source install-deps.sh "ydiff"
+fi
 get_sources
 build_srpm
 build_source_deb

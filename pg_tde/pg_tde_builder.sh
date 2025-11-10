add8b0fdfcaeb76a5715f4788f48df6627e577c3
Manika Singhal <manika.singhal@percona.com>
2025-11-10 20:11:36 +0530

    [PKG-1164]: Add support for pg_tde as an extension

--- pg_tde/pg_tde_builder.sh
+++ pg_tde/pg_tde_builder.sh
@@ -1,98 +1,9 @@
 #!/usr/bin/env bash
-
-shell_quote_string() {
-    echo "$1" | sed -e 's,\([^a-zA-Z0-9/_.=-]\),\\\1,g'
-}
-
-usage () {
-    cat <<EOF
-Usage: $0 [OPTIONS]
-    The following options may be given :
-        --builddir=DIR      Absolute path to the dir where all actions will be performed
-        --get_sources       Source will be downloaded from github
-        --build_src_rpm     If it is set - src rpm will be built
-        --build_src_deb  If it is set - source deb package will be built
-        --build_rpm         If it is set - rpm will be built
-        --build_deb         If it is set - deb will be built
-        --install_deps      Install build dependencies(root privilages are required)
-        --branch            Branch for build
-        --repo              Repo for build
-        --nightly           If it is set - it will be nightly build
-        --help) usage ;;
-Example $0 --builddir=/tmp/BUILD --get_sources=1 --build_src_rpm=1 --build_rpm=1
-EOF
-        exit 1
-}
-
-append_arg_to_args () {
-    args="$args "$(shell_quote_string "$1")
-}
-
-parse_arguments() {
-    pick_args=
-    if test "$1" = PICK-ARGS-FROM-ARGV
-    then
-        pick_args=1
-        shift
-    fi
-
-    for arg do
-        val=$(echo "$arg" | sed -e 's;^--[^=]*=;;')
-        case "$arg" in
-            --builddir=*) WORKDIR="$val" ;;
-            --build_src_rpm=*) SRPM="$val" ;;
-            --build_src_deb=*) SDEB="$val" ;;
-            --build_rpm=*) RPM="$val" ;;
-            --build_deb=*) DEB="$val" ;;
-            --get_sources=*) SOURCE="$val" ;;
-            --branch=*) BRANCH="$val" ;;
-            --repo=*) REPO="$val" ;;
-            --install_deps=*) INSTALL="$val" ;;
-            --nightly=*) NIGHTLY="$val" ;;
-            --help) usage ;;
-            *)
-                if test -n "$pick_args"
-                then
-                    append_arg_to_args "$arg"
-                fi
-            ;;
-        esac
-    done
-}
-
-check_workdir(){
-    if [ "x$WORKDIR" = "x$CURDIR" ]
-    then
-        echo >&2 "Current directory cannot be used for building!"
-        exit 1
-    else
-        if ! test -d "$WORKDIR"
-        then
-            echo >&2 "$WORKDIR is not a directory."
-            exit 1
-        fi
-    fi
-    return
-}
-
-add_percona_yum_repo(){
-    yum -y install https://repo.percona.com/yum/percona-release-latest.noarch.rpm
-    wget https://raw.githubusercontent.com/percona/percona-repositories/release-1.0-28/scripts/percona-release.sh
-    mv percona-release.sh /usr/bin/percona-release
-    chmod 777 /usr/bin/percona-release
-    percona-release disable all
-    percona-release enable ppg-${PG_VERSION} testing
-    return
-}
-
-add_percona_apt_repo(){
-    wget https://repo.percona.com/apt/percona-release_latest.generic_all.deb
-    dpkg -i percona-release_latest.generic_all.deb
-    rm -f percona-release_latest.generic_all.deb
-    percona-release disable all
-    percona-release enable ppg-${PG_VERSION} testing
-    return
-}
+set -x
+# Versions and other variables
+source versions.sh "pg_tde"
+# Common functions
+source common-functions.sh
 
 get_sources(){
     cd "${WORKDIR}"
@@ -101,163 +12,60 @@ get_sources(){
         echo "Sources will not be downloaded"
         return 0
     fi
-    PRODUCT=percona-pg-tde_${PG_MAJOR_VERSION}
 
-    if [ "x$OS" = "xrpm" ]; then
-        PRODUCT=percona-pg_tde_${PG_MAJOR_VERSION}
-    fi
-
-    echo "PRODUCT=${PRODUCT}" > pg_tde.properties
-
-    PRODUCT_FULL=${PRODUCT}-${VERSION}
-    echo "PRODUCT_FULL=${PRODUCT_FULL}" >> pg_tde.properties
-    #echo "VERSION=${PSM_VER}" >> pg_tde.properties
-    echo "VERSION=${VERSION}" >> pg_tde.properties
+    echo "PRODUCT=${PG_TDE_PRODUCT}" > pg_tde.properties
+    echo "PRODUCT_FULL=${PG_TDE_PRODUCT_FULL}" >> pg_tde.properties
+    echo "VERSION=${PG_TDE_VERSION}" >> pg_tde.properties
     echo "BUILD_NUMBER=${BUILD_NUMBER}" >> pg_tde.properties
     echo "BUILD_ID=${BUILD_ID}" >> pg_tde.properties
-    git clone --recursive "$REPO" ${PRODUCT_FULL}
+    git clone --recursive "$PG_TDE_SRC_REPO" ${PG_TDE_PRODUCT_FULL}
     retval=$?
     if [ $retval != 0 ]
     then
         echo "There were some issues during repo cloning from github. Please retry one more time"
         exit 1
     fi
-    cd ${PRODUCT_FULL}
-    if [ ! -z "$BRANCH" ]
+    cd ${PG_TDE_PRODUCT_FULL}
+    if [ ! -z "$PG_TDE_SRC_BRANCH" ]
     then
         git reset --hard
         git clean -xdf
-        git checkout "$BRANCH"
+        git checkout "$PG_TDE_SRC_BRANCH"
     fi
     REVISION=$(git rev-parse --short HEAD)
     echo "REVISION=${REVISION}" >> ${WORKDIR}/pg_tde.properties
     rm -fr debian rpm
 
     #git clone https://salsa.debian.org/postgresql/pg_tde.git deb_packaging
-    mkdir -p deb_packaging/debian
-    cd deb_packaging
+    mkdir debian
+    cd debian
     #git checkout debian/${VERSION}-${RELEASE}
+    wget ${PKG_RAW_URL}/pg_tde/control
+    wget ${PKG_RAW_URL}/pg_tde/control.in
+    wget ${PKG_RAW_URL}/pg_tde/rules
+    wget ${PKG_RAW_URL}/pg_tde/install
     cd ../
-    mv deb_packaging/debian ./
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/pg_tde/control
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/pg_tde/control.in
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/pg_tde/rules
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/pg_tde/install
 
-    rm -rf debian/control*
-    mv control* debian/
-    mv rules debian/
-    mv install debian/
-
-    echo ${PG_MAJOR_VERSION} > debian/pgversions
+    echo ${PG_MAJOR} > debian/pgversions
     echo 10 > debian/compat
     rm -rf deb_packaging
     mkdir rpm
     cd rpm
-    wget https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}/pg_tde/pg_tde.spec
+    wget ${PKG_RAW_URL}/pg_tde/pg_tde.spec
 
     cd ${WORKDIR}
     #
     source pg_tde.properties
     #
 
-    tar --owner=0 --group=0 --exclude=.* -czf ${PRODUCT_FULL}.tar.gz ${PRODUCT_FULL}
+    tar --owner=0 --group=0 --exclude=.* -czf ${PG_TDE_PRODUCT_FULL}.tar.gz ${PG_TDE_PRODUCT_FULL}
     DATE_TIMESTAMP=$(date +%F_%H-%M-%S)
-    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}/${PRODUCT_FULL}/${BRANCH}/${REVISION}/${DATE_TIMESTAMP}/${BUILD_ID}" >> pg_tde.properties
+    echo "UPLOAD=UPLOAD/experimental/BUILDS/${PG_TDE_PRODUCT}/${PG_TDE_PRODUCT_FULL}/${PG_TDE_SRC_BRANCH}/${REVISION}/${DATE_TIMESTAMP}/${BUILD_ID}" >> pg_tde.properties
     mkdir $WORKDIR/source_tarball
     mkdir $CURDIR/source_tarball
-    cp ${PRODUCT_FULL}.tar.gz $WORKDIR/source_tarball
-    cp ${PRODUCT_FULL}.tar.gz $CURDIR/source_tarball
+    cp ${PG_TDE_PRODUCT_FULL}.tar.gz $WORKDIR/source_tarball
+    cp ${PG_TDE_PRODUCT_FULL}.tar.gz $CURDIR/source_tarball
     cd $CURDIR
-    return
-}
-
-get_system(){
-    if [ -f /etc/redhat-release ]; then
-        RHEL=$(rpm --eval %rhel)
-        ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-        OS_NAME="el$RHEL"
-        OS="rpm"
-    else
-        ARCH=$(uname -m)
-        OS_NAME="$(lsb_release -sc)"
-        OS="deb"
-    fi
-    return
-}
-
-install_deps() {
-    if [ $INSTALL = 0 ]
-    then
-        echo "Dependencies will not be installed"
-        return;
-    fi
-    if [ $( id -u ) -ne 0 ]
-    then
-        echo "It is not possible to instal dependencies. Please run as root"
-        exit 1
-    fi
-    CURPLACE=$(pwd)
-
-    if [ "x$OS" = "xrpm" ]; then
-        yum -y install wget
-        add_percona_yum_repo
-        yum clean all
-        RHEL=$(rpm --eval %rhel)
-        if [ x"$RHEL" = x6 -o x"$RHEL" = x7 ]; then
-            until yum -y install centos-release-scl; do
-                echo "waiting"
-                sleep 1
-            done
-            yum -y install epel-release
-            INSTALL_LIST="json-c-devel libcurl-devel openssl-devel gettext git llvm5.0-devel llvm-toolset-7-clang rpmbuild percona-postgresql${PG_MAJOR_VERSION}-devel percona-postgresql${PG_MAJOR_VERSION}-server rpmdevtools vim wget llvm-toolset-7-clang-devel make gcc gcc-c++"
-
-            yum -y install ${INSTALL_LIST}
-            source /opt/rh/devtoolset-7/enable
-            source /opt/rh/llvm-toolset-7/enable
-        else
-            yum -y install epel-release
-            dnf module -y disable postgresql
-            dnf config-manager --set-enabled ol${RHEL}_codeready_builder
-
-            INSTALL_LIST="clang-devel clang llvm-devel json-c-devel libcurl-devel openssl-devel gettext git percona-postgresql${PG_MAJOR_VERSION}-devel percona-postgresql${PG_MAJOR_VERSION}-server rpmdevtools vim wget"
-            yum -y install ${INSTALL_LIST}
-            yum -y install binutils make gcc gcc-c++
-        fi
-    else
-        apt-get -y update
-        apt-get -y install wget lsb-release
-        export DEBIAN=$(lsb_release -sc)
-        export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
-	apt-get -y update || true
-        apt-get -y install gnupg2 curl
-        add_percona_apt_repo
-        #percona-release enable tools testing
-        percona-release enable ppg-${PG_VERSION} testing
-        apt-get update || true
-        INSTALL_LIST="build-essential debhelper clang git libjson-c-dev libcurl4-openssl-dev shtool devscripts"
-        DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated install ${INSTALL_LIST}
-        DEBIAN_FRONTEND=noninteractive apt-get install -y percona-postgresql-common percona-postgresql-server-dev-all
-    fi
-    return;
-}
-
-get_tar(){
-    TARBALL=$1
-    TARFILE=$(basename $(find $WORKDIR/$TARBALL -name 'percona-pg*tde*.tar.gz' | sort | tail -n1))
-    if [ -z $TARFILE ]
-    then
-        TARFILE=$(basename $(find $CURDIR/$TARBALL -name 'percona-pg*tde*.tar.gz' | sort | tail -n1))
-        if [ -z $TARFILE ]
-        then
-            echo "There is no $TARBALL for build"
-            exit 1
-        else
-            cp $CURDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
-        fi
-    else
-        cp $WORKDIR/$TARBALL/$TARFILE $WORKDIR/$TARFILE
-    fi
     return
 }
 
@@ -293,7 +101,7 @@ build_srpm(){
         exit 1
     fi
     cd $WORKDIR
-    get_tar "source_tarball"
+    get_tar "source_tarball" "percona-pg*tde"
     rm -fr rpmbuild
     ls | grep -v tar.gz | xargs rm -rf
     TARFILE=$(find . -name 'percona-pg_tde*.tar.gz' | sort | tail -n1)
@@ -306,8 +114,13 @@ build_srpm(){
     cp -av rpm/pg_tde.spec rpmbuild/SPECS
     #
     mv -fv ${TARFILE} ${WORKDIR}/rpmbuild/SOURCES
-    rpmbuild -bs --define "_topdir ${WORKDIR}/rpmbuild" --define "dist .generic" \
-        --define "version ${VERSION}" --define "release ${BUILD_RELEASE}" rpmbuild/SPECS/pg_tde.spec
+    rpmbuild -bs \
+        --define "_topdir ${WORKDIR}/rpmbuild" \
+        --define "dist .generic" \
+        --define "pgmajor ${PG_MAJOR}" \
+        --define "version ${PG_TDE_VERSION}" \
+        --define "release ${PG_TDE_RELEASE}" \
+        rpmbuild/SPECS/pg_tde.spec
     mkdir -p ${WORKDIR}/srpm
     mkdir -p ${CURDIR}/srpm
     cp rpmbuild/SRPMS/*.src.rpm ${CURDIR}/srpm
@@ -355,7 +168,17 @@ build_rpm(){
         source /opt/rh/devtoolset-7/enable
         source /opt/rh/llvm-toolset-7/enable
     fi
-    rpmbuild --define "_topdir ${WORKDIR}/rpmbuild" --define "dist .$OS_NAME" --define "version ${VERSION}" --define "release ${BUILD_RELEASE}" --rebuild rpmbuild/SRPMS/$SRC_RPM
+
+    if [[ "${RHEL}" -eq 10 ]]; then
+        export QA_RPATHS=0x0002
+    fi
+    rpmbuild \
+        --define "_topdir ${WORKDIR}/rpmbuild" \
+        --define "dist .$OS_NAME" \
+        --define "pgmajor ${PG_MAJOR}" \
+        --define "version ${PG_TDE_VERSION}" \
+        --define "release ${PG_TDE_RELEASE}" \
+        --rebuild rpmbuild/SRPMS/$SRC_RPM
 
     return_code=$?
     if [ $return_code != 0 ]; then
@@ -379,7 +202,7 @@ build_source_deb(){
         exit 1
     fi
     rm -rf percona-pg_tde*
-    get_tar "source_tarball"
+    get_tar "source_tarball" "percona-pg*tde"
     rm -f *.dsc *.orig.tar.gz *.debian.tar.gz *.changes
     #
     TARFILE=$(basename $(find . -name 'percona-pg*tde*.tar.gz' | sort | tail -n1))
@@ -387,9 +210,8 @@ build_source_deb(){
     ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
     tar zxf ${TARFILE}
     BUILDDIR=${TARFILE%.tar.gz}
-    #
-    PRODUCT=percona-pg-tde
-    mv ${TARFILE} ${PRODUCT}_${VERSION}.orig.tar.gz
+
+    mv ${TARFILE} ${PG_TDE_PRODUCT_DEB}_${PG_TDE_VERSION}.orig.tar.gz
     cd ${BUILDDIR}
     rm -f .github/workflows/*.yml
     rm -f .github/workflows/*.yaml
@@ -401,15 +223,15 @@ build_source_deb(){
     rm -rf changelog
     mkdir -p source
     echo "3.0 (quilt)" > source/format
-    echo ${PG_MAJOR_VERSION} > pgversions
+    echo ${PG_MAJOR} > pgversions
     echo 10 > compat
-    echo "percona-pg-tde (${VERSION}-${RELEASE}) unstable; urgency=low" > changelog
+    echo "${PG_TDE_PRODUCT_DEB} (${PG_TDE_VERSION}-${PG_TDE_RELEASE}) unstable; urgency=low" > changelog
     echo "  * Initial Release." >> changelog
     echo " -- Muhammad Aqeel <muhammad.aqeel@percona.com>  $(date -R)" >> changelog
 
     cd ../
     
-    dch -D unstable --force-distribution -v "${VERSION}-${BUILD_RELEASE}" "Update to new pg-tde version ${VERSION}"
+    dch -D unstable --force-distribution -v "${PG_TDE_VERSION}-${PG_TDE_RELEASE}" "Update to new pg-tde version ${PG_TDE_VERSION}"
     dpkg-buildpackage -S
     cd ../
     mkdir -p $WORKDIR/source_deb
@@ -452,10 +274,9 @@ build_deb(){
     DSC=$(basename $(find . -name '*.dsc' | sort | tail -n1))
     #
     dpkg-source -x ${DSC}
-    #
-    PRODUCT=percona-pg-tde
-    cd ${PRODUCT}-${VERSION}
-    dch -m -D "${DEBIAN}" --force-distribution -v "1:${VERSION}-${BUILD_RELEASE}.${DEBIAN}" 'Update distribution'
+
+    cd ${PG_TDE_PRODUCT_DEB}-${PG_TDE_VERSION}
+    dch -m -D "${DEBIAN}" --force-distribution -v "1:${PG_TDE_VERSION}-${PG_TDE_RELEASE}.${DEBIAN}" 'Update distribution'
     unset $(locale|cut -d= -f1)
     dpkg-buildpackage -rfakeroot -us -uc -b
     mkdir -p $CURDIR/deb
@@ -478,35 +299,19 @@ SDEB=0
 RPM=0
 DEB=0
 SOURCE=0
-OS_NAME=
-ARCH=
-OS=
 INSTALL=0
-RPM_RELEASE=2
-DEB_RELEASE=2
 REVISION=0
-BRANCH="1.0.0-beta2"
-PG_VERSION=17.5
-PG_MAJOR_VERSION=$(echo ${PG_VERSION} | cut -f1 -d'.')
-REPO="https://github.com/Percona-Lab/pg_tde.git"
-PRODUCT=percona-pg_tde_${PG_MAJOR_VERSION}
 DEBUG=0
+
 parse_arguments PICK-ARGS-FROM-ARGV "$@"
-VERSION='1.0.0'
-RELEASE='1'
-PRODUCT_FULL=${PRODUCT}-${VERSION}-${RELEASE}
-
-if [ ${NIGHTLY} = 1 ]
-then
-   NIGHTLY_TIMESTAMP=$(date +%Y%m%d%H%M%S)
-   BUILD_RELEASE=${NIGHTLY_TIMESTAMP}.${RELEASE}
-else
-   BUILD_RELEASE=${RELEASE}
-fi
-
 check_workdir
 get_system
-install_deps
+#install_deps
+if [ $INSTALL = 0 ]; then
+    echo "Dependencies will not be installed"
+else
+    source install-deps.sh "pg_tde"
+fi
 get_sources
 build_srpm
 build_source_deb

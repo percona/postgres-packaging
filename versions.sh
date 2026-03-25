b56b5f463bfd6b82465594e9ff68f70bdeb17529
Manika Singhal <manika.singhal@percona.com>
2026-03-25 20:10:07 +0530

    Update branch and raw URL for extras

--- versions.sh
+++ versions.sh
@@ -32,11 +32,11 @@ PG_OIDC_VERSION=1.0
 
 # Github Packaging Repo
 PKG_GIT_REPO="https://github.com/percona/postgres-packaging.git"
-PKG_GIT_BRANCH=${PG_VERSION}
+PKG_GIT_BRANCH=${PG_VERSION}-extras
 PGRPMS_GIT_REPO="https://git.postgresql.org/git/pgrpms.git"
 
 # Raw files URLs
-PKG_RAW_URL="https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}"
+PKG_RAW_URL="https://raw.githubusercontent.com/percona/postgres-packaging/${PG_VERSION}-extras"
 
 # Percona Repos
 YUM_REPO="https://repo.percona.com/yum/percona-release-latest.noarch.rpm"
